6028ac0f22f5601ba3952df752931f1158aff619
RajakumarKalamadi <rajakumarkalamadi7410@gmail.com>
2024-02-04 17:00:14 +0530

    This is my second commit

---
+++ addTwoNumber.java
@@ -0,0 +1,8 @@
+class{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("Addition of Two number is "+ (num1+num2));
+    }
+}
